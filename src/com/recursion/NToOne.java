6a8ccfe4e7d14836a2c582d01319dce17550a4a8
Akash Singh <akashsingh.work@outlook.com>
2020-07-24 21:14:47 +0530

    Added time complexity

--- src/com/recursion/NToOne.java
+++ src/com/recursion/NToOne.java
@@ -1,6 +1,6 @@
 package com.recursion;
 /*Time complexity will be O(0) as we are calling the function for N times
-We are calling the function recursively until N-1 becomes 0 */
+We are calling the function recursively until N becomes 0 */
 public class NToOne {
     static void NToOne(int Num){
         if(Num==0)
